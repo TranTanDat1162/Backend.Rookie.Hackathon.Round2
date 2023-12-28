e32a93dec62a2cadcfcf8840862b23a21dffa82a
TranTanDat1162 <dattt11062002@gmail.com>
2023-12-28 09:35:19 +0700

    Initial creating project

---
+++ src/main/java/com/hackathon/atcollabo_round2/AtcollaboRound2Application.java
@@ -0,0 +1,13 @@
+package com.hackathon.atcollabo_round2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtcollaboRound2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtcollaboRound2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/atcollabo_round2/AtcollaboRound2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.atcollabo_round2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtcollaboRound2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
