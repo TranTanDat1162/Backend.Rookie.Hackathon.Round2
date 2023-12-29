21a4ae5dde80febdf590b297224a39097c1720e9
TranTanDat1162 <dattt11062002@gmail.com>
2023-12-29 10:28:30 +0700

    Comment Code

--- src/main/java/com/hackathon/atcollabo_round2/controller/HelloController.java
+++ src/main/java/com/hackathon/atcollabo_round2/controller/HelloController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
     // http://localhost:8080/hello
+    // It returns to {data} into hello.html
+    // For example: <p th:text="'Hello, ' + ${data}" ></p>
+    // We create hello.html in templates, note static folder in resources folder.
+    // It will return Hello Tấn Dạt!!, which Tấn Đạt!! is the value of the key "data"
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "Tấn Đạt!!");
